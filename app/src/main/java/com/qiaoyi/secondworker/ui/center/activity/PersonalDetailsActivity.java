4112929d472eeba8d719c5690f0efe8bb51914cf
Spirit <15664460099@163.com>
2019-05-07 10:34:58 +0800

    合并冠杰所画页面

--- app/src/main/java/com/qiaoyi/secondworker/ui/center/activity/PersonalDetailsActivity.java
+++ app/src/main/java/com/qiaoyi/secondworker/ui/center/activity/PersonalDetailsActivity.java
@@ -15,6 +15,6 @@ public class PersonalDetailsActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_details);
+        setContentView(R.layout.activity_modify_userinfo);
     }
 }
