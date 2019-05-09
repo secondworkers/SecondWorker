e5d40215ca99abfd76e16fa259cda4fcf3475256
Ling <15290797706@163.com>
2019-05-09 17:11:00 +0800

    xx

--- app/src/main/java/com/qiaoyi/secondworker/TestRunActivity.java
+++ app/src/main/java/com/qiaoyi/secondworker/TestRunActivity.java
@@ -8,7 +8,7 @@ public class TestRunActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_apply_become_secondworker_audit_failure);
+        setContentView(R.layout.activity_order_management);
         //activity_apply_become_secondworker_homepage
         //activity_apply_become_secondworker_fill_information
         //activity_apply_become_secondworker_fill_service_area
@@ -17,5 +17,6 @@ public class TestRunActivity extends AppCompatActivity {
         //activity_apply_become_secondworker_audit_failure
         //activity_apply_become_secondworker_audit_success
         //item_search_service
+        //activity_order_management
     }
 }
